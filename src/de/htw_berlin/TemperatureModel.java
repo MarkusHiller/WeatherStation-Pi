3cf2ea09a53eed411a983a2e5d391f2c7e499175
Valentin <valentin.edikh@googlemail.com>
2015-07-15 13:04:10 +0200

    testing temperature with pi

--- src/de/htw_berlin/TemperatureModel.java
+++ src/de/htw_berlin/TemperatureModel.java
@@ -1,5 +1,8 @@
 package de.htw_berlin;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Date;
 
 public class TemperatureModel extends SensorModel{
@@ -12,6 +15,25 @@ public class TemperatureModel extends SensorModel{
 	}
 	
 	public void getData() {
+		Process pr;
+		try {
+			pr = Runtime.getRuntime().exec("cat /sys/bus/w1/devices/10-000802e41494/w1_slave");
+		
+		BufferedReader in = new BufferedReader(new 
+
+				InputStreamReader(pr.getInputStream()));
+				String line;
+				while ((line = in.readLine()) != null) {
+				    System.out.println(line);
+				}
+				pr.waitFor();
+				System.out.println("ok!");
+
+				in.close();
+		} catch (Exception e){
+			
+		}
+				//string rawData = ;
 		this.sensorid = (int)(Math.random()*(100 - 1));
 		this.sensortype = "Temperature";
 		this.value = Math.random()*(99 - 0);
